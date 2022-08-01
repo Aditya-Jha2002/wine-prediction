aa4b69cce71c6ffe659d974ebf4b5cfd5b8823ac
aditya_jha <4adityajha@gmail.com>
2022-08-01 15:03:16 +0530

    setup done, package created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description="It is a wineq package",
+    author="Aditya Jha",
+    packages=find_packages(),
+    license="MIT"
+)
